afdc0af48869fd64a1d891c844d14980308686cf
Cristian Dean <cristian.dean@corp.globo.com>
2019-07-19 18:24:21 -0300

    Add brew install htop

--- installation.sh
+++ installation.sh
@@ -4,6 +4,8 @@
 
 brew install pt
 brew install zsh
+brew install htop
+
 
 # Installing Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
