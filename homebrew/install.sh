07de1add1287543b37b54e825fb81d6a0405549d
Cristian Dean <cristian.dean@corp.globo.com>
2019-07-23 11:47:44 -0300

    Add homebrew

---
+++ homebrew/install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  fi
+
+fi
+
+# Run Homebrew through the Brewfile
+echo "› brew bundle"
+brew bundle
+
+exit 0
--- installation.sh
+++ installation.sh
@@ -1,14 +1,6 @@
 
 # Install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew install pt
-brew install zsh
-brew install htop
-brew install bat 
-brew install nnn 
-
-
+sh ./homwbrew/install.sh
 
 # Installing Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
